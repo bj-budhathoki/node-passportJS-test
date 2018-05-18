6e48d478d09a4dd8928e4833cf87c32b819f2043
bj-budhathoki <bijayabudhathoki98@gmail.com>
2018-05-18 14:05:17 +0545

    serialize users

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth-routes');
 const passportConfig = require('./config/passport-setup');
-
+const passport = require('passport');
+const cookieSession = require('cookie-session');
 const app = express();
 //set up view engine
 app.set('view engine', 'ejs');
-
+app.use(
+   cookieSession({
+      maxAge: 24 * 60 * 60 * 1000,
+      keys: ['bijaybudathoki']
+   })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 //databse connection
 mongoose.connect('mongodb://localhost/nodeOauth_test', () => {
    console.log('hurrrrrrrrry ! we are connected with databse');
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -23,7 +23,7 @@ router.get(
 // callback route for google to redirect to
 // hand control to passport to use code to grab profile info
 router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
-   res.send('you reached the redirect URI');
+   res.send(req.user);
 });
 
 module.exports = router;
